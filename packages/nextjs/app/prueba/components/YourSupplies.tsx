d97e99930a18fa5336e3afd44865cba46e663f4b
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-07-24 09:49:35 -0600

    refactor (YourSupplies): update YourSupplies component for accurate data display
        - Refactored YourSupplies component to correctly display data extracted from the contract.
        - Implemented useAllowance hook to show the correct balance of assets.
        - Ensured only assets with a balance greater than 0 are displayed.

--- packages/nextjs/app/prueba/components/YourSupplies.tsx
+++ packages/nextjs/app/prueba/components/YourSupplies.tsx
@@ -1,12 +1,13 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Allowance from "./Allowance";
 import IsolatedStateComponent from "@/components/tags/IsolatedState";
 import useAccountAddress from "@/hooks/useAccount";
 import useReadContracts from "@/hooks/useReadContracts";
+import { Address } from "viem";
 
 /**
- * Component for displaying assets that can be supplied.
- * Fetches reserve data and corresponding wallet allowances to display them in a table.
+ * Component for displaying user's supplies.
+ * Fetches reserve data and corresponding allowances to display them in a list.
  */
 const YourSupplies = () => {
   // Hook to fetch reserve data
@@ -16,14 +17,40 @@ const YourSupplies = () => {
 
   // State to store allowances of the assets
   const [allowances, setAllowances] = useState<Record<string, string>>({});
+  // State to store reserves with allowances
+  const [reservesWithAllowances, setReservesWithAllowances] = useState<any[]>([]);
 
   /**
-   * Callback function to handle allowance change.
-   * Updates the state with new allowances.
+   * Callback to handle changes in allowance.
+   * @param {Address} tokenAddress - The address of the token.
+   * @param {string} allowance - The allowance value.
    */
-  const handleAllowanceChange = useCallback((tokenAddress: `0x${string}`, allowance: string) => {
+  const handleAllowanceChange = useCallback((tokenAddress: Address, allowance: string) => {
     setAllowances(prevAllowances => ({ ...prevAllowances, [tokenAddress]: allowance }));
   }, []);
+
+  // Effect to update reserves with allowances when reserve data or allowances change
+  useEffect(() => {
+    if (reserveData) {
+      const updatedReserves = reserveData.map(reserve => ({
+        ...reserve,
+        allowance: allowances[reserve.underlyingAsset as Address] || "0",
+      }));
+      setReservesWithAllowances(updatedReserves);
+    }
+  }, [reserveData, allowances]);
+
+  // Effect to log reserves with allowances when they are updated
+  // useEffect(() => {
+  //   if (reservesWithAllowances.length > 0) {
+  //     console.log("Reserves with Allowances:", reservesWithAllowances);
+  //     reservesWithAllowances.forEach(reserve => {
+  //       if (reserve.allowance > 0) {
+  //         console.log("Allowance for", reserve.symbol, ":", reserve.allowance);
+  //       }
+  //     });
+  //   }
+  // }, [reservesWithAllowances]);
 
   return (
     <div className="mt-4">
@@ -31,78 +58,71 @@ const YourSupplies = () => {
       {isLoadingReserveData && <p className="text-amber-950">Loading...</p>}
       {isErrorReserveData && <p className="text-error">Error fetching data.</p>}
 
-      {/* Display table if reserve data is available */}
-      {reserveData && walletAddress && (
-        <div>
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead>
-              <tr className="text-center">
-                <th scope="col" className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Assets
-                </th>
-                <th scope="col" className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Wallet Allowance
-                </th>
-                <th scope="col" className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  APY
-                </th>
-                <th scope="col" className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Collateral
-                </th>
-                <th scope="col" className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Actions
-                </th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200 text-center">
-              {/* Iterate through reserve data to create table rows */}
-              {reserveData.map((reserve, index) => {
-                const allowance = allowances[reserve.underlyingAsset as `0x${string}`];
-                const isButtonDisabled = !allowance || parseFloat(allowance) === 0;
+      {/* Display supplies if reserve data is available */}
+      {reservesWithAllowances.length > 0 && walletAddress ? (
+        <div className="supplies-container">
+          {/* Header for supplies table */}
+          <div className="supplies-header py-3 flex text-center justify-between text-xs font-medium text-gray-500 uppercase tracking-wider border-b border-b-gray-200">
+            <div className="supplies-header-item w-24">Assets</div>
+            <div className="supplies-header-item w-24">Balance</div>
+            <div className="supplies-header-item w-24">APY</div>
+            <div className="supplies-header-item w-24">Collateral</div>
+            <div className="supplies-header-item w-24">Actions</div>
+          </div>
+          {/* Iterate through reserves with allowances to create rows */}
+          {reservesWithAllowances.map((reserve, index) => {
+            if (reserve.allowance === "NaN" || reserve.allowance === 0) {
+              return null;
+            }
 
-                return (
-                  <tr key={index}>
-                    <td className="px-6 py-4">
-                      <p className="text-sm font-medium text-gray-900">{reserve.symbol}</p>
-                    </td>
-                    <td className="px-6 py-4">
-                      {/* Use Allowance to display the allowance */}
-                      <Allowance
-                        tokenAddress={reserve.underlyingAsset as `0x${string}`}
-                        ownerAddress={walletAddress}
-                        spenderAddress={walletAddress} // Adjust this if needed
-                        onAllowanceChange={handleAllowanceChange}
-                      />
-                    </td>
-                    <td className="px-6 py-4">
-                      <p className="text-sm text-gray-900">{(Number(reserve.liquidityRate) / 1e25).toFixed(2)}%</p>
-                    </td>
-                    <td className="px-6 py-4">
-                      <div className="text-sm text-gray-900">
-                        {/* Display collateral status */}
-                        {reserve.usageAsCollateralEnabled ? (
-                          <span className="text-xl text-success font-bold">&#10003;</span>
-                        ) : (
-                          <IsolatedStateComponent message="Isolated" />
-                        )}
-                      </div>
-                    </td>
-                    <td className="py-4 whitespace-nowrap text-sm font-medium">
-                      <button
-                        className={`px-3 py-1 rounded-md ${
-                          isButtonDisabled ? "bg-gray-100 text-gray-300 cursor-not-allowed" : "bg-accent text-white"
-                        }`}
-                        disabled={isButtonDisabled}
-                      >
-                        Withdraw
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+            const isButtonDisabled = parseFloat(reserve.allowance) === 0;
+
+            return (
+              <div
+                key={index}
+                className={`supplies-row flex justify-between py-3 text-sm text-center ${
+                  isButtonDisabled ? "hidden" : "block"
+                }`}
+              >
+                <div className="supplies-row-item w-24">
+                  <p className="text-sm font-medium text-gray-900">{reserve.symbol}</p>
+                </div>
+                <div className="supplies-row-item w-24">
+                  <Allowance
+                    tokenAddress={reserve.underlyingAsset as Address}
+                    ownerAddress={walletAddress}
+                    spenderAddress={walletAddress}
+                    onAllowanceChange={handleAllowanceChange}
+                  />
+                </div>
+                <div className="supplies-row-item w-24">
+                  <p className="text-sm text-gray-900">{(Number(reserve.liquidityRate) / 1e25).toFixed(2)}%</p>
+                </div>
+                <div className="supplies-row-item w-24">
+                  <div className="text-sm text-gray-900">
+                    {reserve.usageAsCollateralEnabled ? (
+                      <span className="text-xl text-success font-bold">&#10003;</span>
+                    ) : (
+                      <IsolatedStateComponent message="Isolated" />
+                    )}
+                  </div>
+                </div>
+                <div className="supplies-row-item w-24">
+                  <button
+                    className={`px-3 py-1 rounded-md ${
+                      isButtonDisabled ? "bg-gray-100 text-gray-300 cursor-not-allowed" : "bg-accent text-white"
+                    }`}
+                    disabled={isButtonDisabled}
+                  >
+                    Withdraw
+                  </button>
+                </div>
+              </div>
+            );
+          })}
         </div>
+      ) : (
+        <p className="text-center text-gray-500">No sufficient data available.</p>
       )}
     </div>
   );
