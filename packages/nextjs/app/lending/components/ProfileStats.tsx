2898ab98a8fe5433612908ee75d19f600c9cc745
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2024-09-18 10:06:20 +0200

    Added network error in Lending

--- packages/nextjs/app/lending/components/ProfileStats.tsx
+++ packages/nextjs/app/lending/components/ProfileStats.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { useTranslation } from "@/app/context/LanguageContext";
 import BaseLogo from "@/public/Base-Logo.jpg";
+import { useChainId } from "wagmi";
 
 interface ProfileStatsProps {
   balance: number;
@@ -11,23 +12,38 @@ interface ProfileStatsProps {
 
 const ProfileStats: React.FC<ProfileStatsProps> = ({ balance, netAPY, healthFactor }) => {
   const { t } = useTranslation();
+  const chainId = useChainId();
   const data = {
     netWorth: balance.toFixed(2),
     netAPY: netAPY.toFixed(2),
     healthFactor: healthFactor.toFixed(2),
   };
 
+  // Function to get network error message based on chainId
+  const getNetworkErrorMessage = () => {
+    if (chainId === 56 || chainId === 137) {
+      return t("WrongNetworkMessage"); // Replace with your translation key
+    }
+    return null;
+  };
+
+  const networkErrorMessage = getNetworkErrorMessage();
+
   return (
     <header className="bg-inherit dark:bg-inherit flex flex-col space-y-2 w-full md:w-fit">
       <div>
-        <div className="flex items-center space-x-2 mb-2">
-          <div className="text-2xl">
-            <Image src={BaseLogo} alt="Base Logo" className="h-8 w-8" />
+        {networkErrorMessage ? (
+          <div className="text-2xl text-red-600 font-bold">Wrong network, change to the Base network!</div>
+        ) : (
+          <div className="flex items-center space-x-2 mb-2">
+            <div className="text-2xl">
+              <Image src={BaseLogo} alt="Base Logo" className="h-8 w-8" />
+            </div>
+            <div>
+              <div className="text-2xl text-primary font-semibold">Base Market</div>
+            </div>
           </div>
-          <div>
-            <div className="text-2xl text-primary font-semibold">Base Market</div>
-          </div>
-        </div>
+        )}
         <div className="flex flex-col md:flex-row items-start md:items-center space-y-4 md:space-y-0 md:space-x-8">
           <div className="text">
             <div className="text-sm text-gray-400">{t("LendingProfileNetWorth")}</div>
