2d94a4d7f7e40db827e95386c163ce6c107bdc84
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2024-10-10 19:26:01 +0200

    fixed RepayModal

--- packages/nextjs/hooks/useRepayCDP.ts
+++ packages/nextjs/hooks/useRepayCDP.ts
@@ -1,12 +1,14 @@
 import { houseOfCoinABI } from "@/app/components/abis/houseofcoin";
-import { parseEther } from "viem";
+import { Address, parseEther } from "viem";
 import { useWriteContract } from "wagmi";
+
+// import { houseOfCoinContract } from "~~/app/constants/contracts";
 
 /**
  * Custom hook to handle repay transactions.
  * @returns {Object} - The repay handler function and contract interaction states.
  */
-const useRepay = () => {
+const useRepayCPD = () => {
   // Hook for writing to a smart contract
   const { writeContract, error, data, isPending, isSuccess } = useWriteContract();
 
@@ -15,7 +17,7 @@ const useRepay = () => {
    * @param {string} backedTokenID - The ID of the backed token.
    * @param {string} amount - The amount to repay in ether.
    */
-  const handleRepay = (backedTokenID: string, amount: string) => {
+  const handleRepay = (houseOfCoinContract: Address, backedTokenID: string, amount: string) => {
     if (!houseOfCoinABI || !backedTokenID || !amount) {
       console.error("Required parameters are not properly defined.");
       return;
@@ -23,7 +25,7 @@ const useRepay = () => {
 
     try {
       writeContract({
-        address: "0x7ed1aCD46dE3a4E63f2D3b0f4fB5532e113a520B",
+        address: houseOfCoinContract as Address,
         abi: houseOfCoinABI,
         functionName: "paybackCoin",
         args: [backedTokenID, parseEther(amount)],
@@ -36,4 +38,4 @@ const useRepay = () => {
   return { handleRepay, isError: !!error, error, data, isPending, isSuccess };
 };
 
-export default useRepay;
+export default useRepayCPD;
