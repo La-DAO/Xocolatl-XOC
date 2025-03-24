d50a21920d2de68966343abb8ef103b85c8f6929
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2025-03-24 13:01:58 +0100

    adjustment

--- packages/nextjs/app/directory/page.tsx
+++ packages/nextjs/app/directory/page.tsx
@@ -131,8 +131,8 @@ const Directory = () => {
             <button onClick={closeModal} className="absolute text-4xl top-4 right-4 text-gray-500 hover:text-gray-700">
               &times;
             </button>
-            <h1 className="text-4xl font-bold mb-4">{selectedUseCase.title}</h1>
-            <p className="mb-4">{selectedUseCase.description}</p>
+            <h1 className="text-4xl text-primary font-bold mb-4">{selectedUseCase.title}</h1>
+            <p className="mb-4 text-primary">{selectedUseCase.description}</p>
             {/* Carousel placeholder */}
             <div className="mb-4">
               <Image
