12371aace49b30955b22a390bbac9a56933bff18
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2025-05-16 11:15:47 +0200

    updated Repay flow

--- packages/nextjs/app/lending/components/modals/SupplyTransactionModal.tsx
+++ packages/nextjs/app/lending/components/modals/SupplyTransactionModal.tsx
@@ -50,6 +50,7 @@ const SupplyTransactionModal: React.FC<ModalProps> = ({ isOpen, onClose, reserve
 
   useEffect(() => {
     validateAmount(amount);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [amount]);
 
   useEffect(() => {
